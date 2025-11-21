90ae58b50f896dbbebe5a5dcbe956fbef8d832a7
Rany-Touffaha <r_touffaha@hotmail.com>
2025-11-21 15:21:57 +0100

    Update GLFW initialization helper with error callback using spdlog
    - Register a custom GLFW error callback that logs errors using spdlog
    - Call glfwInit() with failure handling and immediate process exit on initialization failure

--- src/glfw_initialization.cpp
+++ src/glfw_initialization.cpp
@@ -1,11 +1,19 @@
 #include <GLFW/glfw3.h>
 #include <glfw_initialization.h>
 #include <precomp.h>
+#include <spdlog/spdlog.h>
 
 namespace vulkanEng
 {
+    void glfw_error_callback(std::int32_t error_code, gsl::czstring message)
+    {
+        spdlog::error("GLFW Error ({}): {}", error_code, message);
+    }
+
     GLFWInitialization::GLFWInitialization()
     {
+        glfwSetErrorCallback(glfw_error_callback);
+
         if (glfwInit() != GLFW_TRUE)
         {
             std::exit(EXIT_FAILURE);
