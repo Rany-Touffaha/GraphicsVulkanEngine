5c94f11130eb0e72ac1180a4bbb8b31e071c56d2
Rany-Touffaha <r_touffaha@hotmail.com>
2025-11-22 20:51:45 +0100

    Update the CMake shader pipeline and introduce initial GLSL shader files
    - Add basic fragment shader using common.glsl and outputting a constant color
    - Add vertex shader with hardcoded triangle positions and Vulkan GLSL extensions enabled
    - Refactor add_shaders to generate proper SPIR-V outputs with add_custom_command and explicit dependencies
    - Clean up duplicated error messages and ensure shader output paths are consistently defined

--- cmake/shaders.cmake
+++ cmake/shaders.cmake
@@ -1,34 +1,32 @@
 function(add_shaders TARGET_NAME)
+
     set(SHADER_SOURCE_FILES ${ARGN})
     list(LENGTH SHADER_SOURCE_FILES FILE_COUNT)
     if(FILE_COUNT EQUAL 0)
-        message(FATAL_ERROR "Cannot add shaders target without shader source files.")        
+        message(FATAL_ERROR "Cannot add shaders target without shader source files.")
     endif()
 
-    set(SHADER_COMMANDS)
     set(SHADER_PRODUCTS)
 
     foreach(SHADER_SOURCE IN LISTS SHADER_SOURCE_FILES)
         cmake_path(ABSOLUTE_PATH SHADER_SOURCE NORMALIZE)
         cmake_path(GET SHADER_SOURCE FILENAME SHADER_NAME)
+        set(SHADER_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${SHADER_NAME}.spv")
 
         # COMMANDS
-        list(APPEND SHADER_COMMANDS COMMAND)
-        list(APPEND SHADER_COMMANDS Vulkan::glslc ARGS
-            "${SHADER_SOURCE}"
-            "-o" "${CMAKE_CURRENT_BINARY_DIR}/${SHADER_NAME}.spv"
+        add_custom_command(
+            OUTPUT ${SHADER_OUTPUT}
+            COMMAND Vulkan::glslc "${SHADER_SOURCE}" -o "${SHADER_OUTPUT}"
+            DEPENDS ${SHADER_SOURCE}
+            COMMENT "Compiling shader: ${SHADER_NAME}"
         )
         # PRODUCTS
-        list(APPEND SHADER_PRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/${SHADER_NAME}.spv")
-
-
+        list(APPEND SHADER_PRODUCTS ${SHADER_OUTPUT})
     endforeach()
-    
+
     add_custom_target(${TARGET_NAME} ALL
-        ${SHADER_COMMANDS}
-        COMMENT "Compiling shaders..."
-        SOURCES ${SHADER_SOURCE_FILES}
-        BYPRODUCTS ${SHADER_PRODUCTS}
+        DEPENDS ${SHADER_PRODUCTS}
+        COMMENT "Compiling all shaders..."
     )
-    
+
 endfunction()
